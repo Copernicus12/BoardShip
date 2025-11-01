b67b17a76173234e98a7ccd56384369087d5c621
Copernicus12 <razvan.hosu32@gmail.com>
2025-11-01 22:00:20 +0200

    first commit

---
+++ src/main/java/com/boardship/backend/BoardShipApplication.java
@@ -0,0 +1,13 @@
+package com.boardship.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardShipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardShipApplication.class, args);
+    }
+
+}
